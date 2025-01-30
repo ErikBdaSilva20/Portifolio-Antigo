a9caeb17f036863a0ab31f32101d59eb7f78375f
ErikDaSilva19 <erikborgesdasilva999@gmail.com>
2025-01-29 21:58:39 -0300

    Ajustes da url

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GlobalStyles } from "./styles/GlobalStyles.js";
-import { BrowserRouter } from "react-router-dom";
 import router from "./routes.jsx";
 import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStyles />
-    <BrowserRouter basename="/Minha_Pag_Portifolio"> {/* Define o basename do site */}
-      <RouterProvider router={router} />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
